737c3ac2923c07792e7518ca310fbd72b6e095ea
Olzhas <olzhas.dossaibek.dev@gmail.com>
2024-11-13 18:28:36 +0500

    188 - temp proj code, 189 - theory slides

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import DateCounter from "./DateCounter";
 export default function App() {
   return (
     <div>
-      <DateCounter></DateCounter>
+      <h3>188 - temp proj code</h3>
+      <h3>189 - theory slides</h3>
+      <h3>190 - next to code</h3>
     </div>
   );
 }
