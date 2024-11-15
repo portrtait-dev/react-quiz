7c29b981d3784cbf184eda342ef1ac954eeb4add
type sh,t <olzhas.dossaibek.dev@gmail.com>
2024-11-15 12:07:26 +0500

    193. Starting a New Quiz
    Desired behavior:
    1. Click on the ‘Let’s Start’ button → Open new Component (Question)
    
    Underlying implementation:
    1.1. JSX: Add new logical operator to UI (status and Component) – create new Component – pass Dispatcher as a prop – call Dispatcher on click
    1.2. Non-JSX: Create new Component [Question] – create new case in Reducer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -26,6 +27,13 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+
     default:
       throw new Error("Action unknown");
   }
@@ -50,7 +58,11 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
