7d120355b838cad9eabb156a0863ab3fd18289ec
type sh,t <olzhas.dossaibek.dev@gmail.com>
2024-11-16 15:04:24 +0500

    196. Moving to the Next Question - button appears and button works - created new case and new Component - new questions are reset AND points are calculating correctly
    Desired behavior:
    1. Choose the answer → show the ‘Next’ button
    2. Click on ‘Next’ button → show the next question
    
    Underlying implementation:
    1. Add one more Component for the ‘active’ status in Main Component  – create this new Component (‘NextButton’) – inside the new Component, do logic to show it only if we receive answer to the question (using the Prop)
    2. Dispatch new case (‘nextQuestion’) – create this new case in the Reducer – inside the case update index and answer – points are being calculated properly automatically

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 
 const initialState = {
   questions: [],
@@ -49,6 +50,9 @@ function reducer(state, action) {
             : state.points,
       };
 
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
     default:
       throw new Error("Action unknown");
   }
@@ -81,11 +85,15 @@ export default function App() {
         )}
 
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
