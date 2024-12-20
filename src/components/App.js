71fd094648642ef182d5347822d554da0e7c5102
type sh,t <olzhas.dossaibek.dev@gmail.com>
2024-12-20 11:55:19 +0500

    p.2 - 241. CHALLENGE #2: Refactoring ‘React Quiz’ to Context API
    - xxx
    
    - in this lecture, practice Context API - refactor our React Quiz app for the Context API
    - next section - it will be a bit complicated deidi, but this is why we are here deidi
    
    Desired behavior:
    - Refactor the React Quiz App for Context API + Reducer
    -- The benefit is - we get rid of all this props in App.js and our code gets far cleaner and straightforward
    -- The idea is - delete all props and instead get them via Context API
    
    Underlying implementation:
    1. Make a copy of the old solution which is without Context
    2. Review code and its behavior
    3. Prepare for the solution
    - Worldwise App - Creating a CitiesContext - https://github.com/portrtait-dev/worldwise/commit/9bfd467922d04d81d2e30e606a5c18704a464d44 - the most important part
    ✅1. Create new - CitiesProvider, useCities, export
    ✅2. useCities: pretty standard and boilerplate thing
    🔁3. CitiesProvider: return part - pretty standard and boilerplate, value = list all props
    🔁4. CitiesProvider: non-return part - all necessary logic, cut-paste of all the App.jsx’s non-return code,
    
    - Worldwise App - Consuming the CitiesContext - https://github.com/portrtait-dev/worldwise/commit/9b2dd8c2daf0bb5401ea11e0ade57ddf457ee3dd - easy - remove props, instead consume values from Context’s Hook, wrap into CitiesProvider
    5. Start implementation
    -- 1. Create new file (QuizContext.jsx) and fill its starter code (createContext(), children prop, Context.Provider, useContext(), if undefined)
    -- 2. Cut-paste all the non-return-part code from App.js into QuizContext.js
    -- 3. Consume status in the App.js
    
    Next:
    - double check whether non-return code (dispatches and reducer) all work well
    - put derived States where required
    - delete props in App.js
    - consume values in each Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+// import { useEffect, useReducer } from "react";
 
 import Header from "./Header";
 import Main from "./Main";
@@ -11,104 +11,108 @@ import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
+import { useQuiz } from "../contexts/QuizContext";
 
-const SECS_PER_QUESTION = 30;
+// const SECS_PER_QUESTION = 30;
 
-const initialState = {
-  questions: [],
+// const initialState = {
+//   questions: [],
 
-  // 'loading', 'error', 'ready', 'active', 'finished'
-  status: "loading",
-  index: 0,
-  answer: null,
-  points: 0,
-  highscore: 0,
-  secondsRemaining: null,
-};
+//   // 'loading', 'error', 'ready', 'active', 'finished'
+//   status: "loading",
+//   index: 0,
+//   answer: null,
+//   points: 0,
+//   highscore: 0,
+//   secondsRemaining: null,
+// };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "dataReceived":
-      return {
-        ...state,
-        questions: action.payload,
-        status: "ready",
-      };
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "dataReceived":
+//       return {
+//         ...state,
+//         questions: action.payload,
+//         status: "ready",
+//       };
 
-    case "dataFailed":
-      return {
-        ...state,
-        status: "error",
-      };
+//     case "dataFailed":
+//       return {
+//         ...state,
+//         status: "error",
+//       };
 
-    case "start":
-      return {
-        ...state,
-        status: "active",
-        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
-      };
+//     case "start":
+//       return {
+//         ...state,
+//         status: "active",
+//         secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+//       };
 
-    case "newAnswer":
-      const question = state.questions.at(state.index);
+//     case "newAnswer":
+//       const question = state.questions.at(state.index);
 
-      return {
-        ...state,
-        answer: action.payload,
-        points:
-          action.payload === question.correctOption
-            ? state.points + question.points
-            : state.points,
-      };
+//       return {
+//         ...state,
+//         answer: action.payload,
+//         points:
+//           action.payload === question.correctOption
+//             ? state.points + question.points
+//             : state.points,
+//       };
 
-    case "nextQuestion":
-      return { ...state, index: state.index + 1, answer: null };
+//     case "nextQuestion":
+//       return { ...state, index: state.index + 1, answer: null };
 
-    case "finish":
-      return {
-        ...state,
-        status: "finished",
-        highscore:
-          state.points > state.highscore ? state.points : state.highscore,
-      };
+//     case "finish":
+//       return {
+//         ...state,
+//         status: "finished",
+//         highscore:
+//           state.points > state.highscore ? state.points : state.highscore,
+//       };
 
-    case "restart":
-      return {
-        ...initialState,
-        questions: state.questions,
-        highscore: state.highscore,
-        status: "ready",
-      };
+//     case "restart":
+//       return {
+//         ...initialState,
+//         questions: state.questions,
+//         highscore: state.highscore,
+//         status: "ready",
+//       };
 
-    case "tick":
-      return {
-        ...state,
-        secondsRemaining: state.secondsRemaining - 1,
-        status: state.secondsRemaining === 0 ? "finished" : state.status,
-      };
+//     case "tick":
+//       return {
+//         ...state,
+//         secondsRemaining: state.secondsRemaining - 1,
+//         status: state.secondsRemaining === 0 ? "finished" : state.status,
+//       };
 
-    default:
-      throw new Error("Action unknown");
-  }
-}
+//     default:
+//       throw new Error("Action unknown");
+//   }
+// }
 
 export default function App() {
-  const [
-    { status, questions, index, answer, points, highscore, secondsRemaining },
-    dispatch,
-  ] = useReducer(reducer, initialState);
+  // const [
+  //   { status, questions, index, answer, points, highscore, secondsRemaining },
+  //   dispatch,
+  // ] = useReducer(reducer, initialState);
 
+  // THESE ARE DERIVED STATES, PUT THEM IN THE TARGETED COMPONENTS
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
     (prev, cur) => prev + cur.points,
     0
   );
 
-  useEffect(function () {
-    fetch("http://localhost:9000/questions")
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => dispatch({ type: "dataFailed" }));
-  }, []);
+  // useEffect(function () {
+  //   fetch("http://localhost:9000/questions")
+  //     .then((res) => res.json())
+  //     .then((data) => dispatch({ type: "dataReceived", payload: data }))
+  //     .catch((err) => dispatch({ type: "dataFailed" }));
+  // }, []);
+
+  const { status } = useQuiz();
 
   return (
     <div className="app">
